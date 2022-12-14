b40f47876517e2d41d2c931aeb0c970c646bbfa8
isaacbaccah <isaacbaccah@icloud.com>
2022-12-13 23:39:13 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in /tmp
 
-file { 'holberton':
+file { 'school':
   ensure  => 'present',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
 }
